79ce8d38c33945171521c444ee4f115c7bbf55b1
Guto Motta <gutomotta@outlook.com.br>
2017-09-20 22:15:20 -0300

    fixes in Chord Matcher and also in template builder (new template built, yay)

--- src/chord_matcher/chord_matcher.rb
+++ src/chord_matcher/chord_matcher.rb
@@ -8,16 +8,21 @@ class ChordMatcher
     config = YAML.load_file(config_file)
     @shorthands = config["shorthands"]
     @intervals = config["intervals"]
+
     pitches = %w(C C# D D# E F F# G G# A A# B)
-    @pitch_index = 12.times.map { |i| [pitches[i], i] }.to_h
+    @pitch_index = pitches.map.with_index.to_h
+
+    @main_chord_templates = {}
+    pitches.each do |pitch|
+      %w(maj min dim aug).each do |shorthand|
+        label = [pitch, shorthand != "maj" ? shorthand : nil].compact.join(":")
+        @main_chord_templates[notes(label)] = label
+      end
+    end
   end
 
-  def reduce(chord)
-    pitch, nts = notes(chord)
-    %w(maj min dim aug).each do |s|
-      return "#{pitch}:#{s}" if nts.first(3) == @shorthands[s]
-    end
-    "N"
+  def normalize(chord)
+    @main_chord_templates[notes(chord)]
   end
 
   def compare(chord_a, chord_b)
@@ -29,31 +34,43 @@ class ChordMatcher
   def notes(chord)
     return [nil, []] if chord == "N"
 
-    pitch = parse_pitch(chord)
-    notes = parse_shorthand(chord)
-    added_notes, removed_notes = parse_intervals(chord)
-    bass_notes = parse_bass(chord)
-    notes = notes + added_notes - removed_notes + bass_notes
-    notes += @shorthands["maj"] if notes.empty?
+    notes = Array.new(12, 0)
 
-    [pitch, notes.sort]
+    root = parse_pitch(chord)
+    added_notes, removed_notes = parse_intervals(root, chord)
+    shorthand_notes = parse_shorthand(root, chord)
+
+    if [added_notes + shorthand_notes].flatten.size == 0
+      added_notes = parse_shorthand(root, ":maj")
+    end
+
+    indices_to_add = [
+      root, shorthand_notes,
+      parse_bass(root, chord),
+      added_notes
+    ].flatten.uniq
+
+    indices_to_add.each { |i| notes[i] = 1 }
+    removed_notes.each { |i| notes[i] = 0 }
+
+    notes
   end
 
   def parse_pitch(chord)
     pitch = chord.split(":")[0]
     pitch = pitch.split("/")[0]
     root = pitch.delete("b#")
-    @pitch_index[root] + pitch.count("#") - pitch.count("b")
+    (@pitch_index[root] + pitch.count("#") - pitch.count("b")) % 12
   end
 
-  def parse_shorthand(chord)
+  def parse_shorthand(root, chord)
     shorthand = chord[/(?<=:)[^()\/]+/]
     notes = @shorthands[shorthand]
     p "ERROR! Invalid shorthand: #{shorthand}" if shorthand && notes.nil?
-    notes.to_a
+    sum_root(root, notes.to_a)
   end
 
-  def parse_intervals(chord)
+  def parse_intervals(root, chord)
     added_notes = []
     removed_notes = []
 
@@ -67,17 +84,21 @@ class ChordMatcher
       end
     end
 
-    [added_notes, removed_notes]
+    [sum_root(root, added_notes), sum_root(root, removed_notes)]
   end
 
   def parse_interval(interv)
     i = interv[/\d+/].to_i
     i %= 7 if i > 7
-    @intervals[i] + interv.count("#") - interv.count("b")
+    (@intervals[i] + interv.count("#") - interv.count("b")) % 12
   end
 
-  def parse_bass(chord)
+  def parse_bass(root, chord)
     bass = chord[/(?<=\/).+$/]
-    bass ? [parse_interval(bass)] : []
+    bass ? sum_root(root, [parse_interval(bass)]) : []
+  end
+
+  def sum_root(root, notes)
+    notes.map { |note| (note + root) % 12 }
   end
 end
--- src/template_builder/build_chord_templates.rb
+++ src/template_builder/build_chord_templates.rb
@@ -1,10 +1,12 @@
+require 'pp'
+
 require File.expand_path("../../file_list/file_list.rb", __FILE__)
 require File.expand_path("../../chord_matcher/chord_matcher.rb", __FILE__)
 require File.expand_path("../../annotation_loop/annotation_loop.rb", __FILE__)
 
 include AnnotationLoop
 
-directory_scope = ARGV[0]
+directory_scope = ARGV[0] || "tmp"
 
 def read_chroma_file(path)
   lines = file_lines(path)
@@ -27,23 +29,50 @@ def file_lines(filename)
 end
 
 file_list = FileList.new(directory_scope)
-
 n = file_list.size
 
-chord_arrays = Hash.new { |h, k| h[k] = [] }
-matcher = ChordMatcher.new
+normalizer = ChordMatcher.new
 
-n.times do |i|
-  p i
-  ann_chroma = read_chroma_file(file_list.chroma_files[i])
-  ann_gt = read_gt_file(file_list.gt_files[i])
+chord_arrays = Hash.new { |h, k| h[k] = [] }
+
+train_indices = n.times.to_a
+test_indices = []
+(n / 4).times do
+  test_indices << train_indices.delete_at(rand(train_indices.size))
+end
+
+train_indices.each_with_index do |t, i|
+  print "#{(100.0 * i / train_indices.size).round(2)}%\n"
+  ann_chroma = read_chroma_file(file_list.chroma_files[t])
+  ann_gt = read_gt_file(file_list.gt_files[t])
 
   annotation_loop(ann_gt, ann_chroma) do |chord, chroma|
-    reduced_chord = matcher.reduce(chord)
-    if reduced_chord != "N"
-      chord_arrays[reduced_chord] += chroma
-    end
+    chord_arrays[normalizer.normalize(chord) || chord] << chroma
   end
 end
 
-p chord_arrays
+filtered = chord_arrays.select { |chord, array|
+  %w(C C:min C# C#:min D D:min D# D#:min
+     E E:min F F:min F# F#:min  G  G:min
+     G# G#:min A A:min A# A#:min B B:min).include? chord
+}
+
+def avg(arrays)
+  n = arrays.size
+  sum = arrays.inject(Array.new(12, 0)) do |accumulated, next_array|
+    12.times.map { |i| accumulated[i] + next_array[i] }
+  end
+  sum.map { |e| 1.0 * e / n }
+end
+
+templates = filtered.map { |chord, arrays| [chord, avg(arrays)] }
+
+File.open("templates_#{directory_scope}.yml", "w") do |file|
+  templates.each do |chord, template|
+    file << "\"#{chord}\": #{template}\n"
+  end
+end
+
+File.open("templates_test_files.yml", "w") do |file|
+  test_indices.each { |i| file << "#{file_list.audio_files[i]}\n" }
+end
