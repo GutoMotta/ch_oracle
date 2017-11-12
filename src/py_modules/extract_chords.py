63d58cf2004ac09b55efd0c7963c653c2945eeae
Guto Motta <gutomotta@outlook.com.br>
2017-11-12 19:23:26 -0200

    Removes files that will be no longer used

--- src/py_modules/extract_chords.py
+++
@@ -1,9 +0,0 @@
-from ch_oracle import ChOracle
-import sys
-
-if len(sys.argv) > 4:
-    t = float(sys.argv[4])
-else:
-    t = None
-
-ChOracle(sys.argv[2], sys.argv[1], t).save_evaluation_file(sys.argv[3])
