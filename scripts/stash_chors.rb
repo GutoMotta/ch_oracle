0dd2685d9ffa63a1575d68aa8f765fe93f3b6428
Guto Motta <gutomotta@outlook.com.br>
2017-11-13 21:06:58 -0200

    Adds script for stashing data for temporary tests

---
+++ scripts/stash_chors.rb
@@ -0,0 +1,50 @@
+@directories_to_stash = %w(chords chromagrams experiments figs templates)
+
+def already_stashed?
+  @directories_to_stash.map { |dir| File.directory?("old_#{dir}") }.any?
+end
+
+def stash
+  @directories_to_stash.each do |dir|
+    if File.directory?(dir)
+      puts "#{dir} => old_#{dir}"
+      File.rename(dir, "old_#{dir}")
+    end
+  end
+end
+
+def existing_directories
+  @directories_to_stash.select { |dir| File.directory?(dir) }
+end
+
+def pop
+  @directories_to_stash.each do |dir|
+    if File.directory?("old_#{dir}")
+      puts "old_#{dir} => #{dir}"
+      File.rename("old_#{dir}", dir)
+    end
+  end
+end
+
+
+case ARGV[0]
+when nil
+  if already_stashed?
+    raise 'There are stashed directories that would be overwritten.'
+  end
+
+  stash
+when 'pop'
+  if existing_directories.any?
+    puts "This is a destructive action and will delete these directories:\n\n"
+    puts existing_directories.join(', ')
+    puts "\nContinue? (anything other than 'y' will stop this script)"
+
+    if STDIN.gets.strip != 'y'
+      puts 'Operation cancelled.'
+      exit
+    end
+  end
+
+  pop
+end
